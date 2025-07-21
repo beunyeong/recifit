54b8a248609228a194cc1e7bef59835e355ed626
beunyeong <beunyeong.b@gmail.com>
2025-07-21 15:40:25 +0900

    Feat: 프로젝트 생성 및 JDK 설정

---
+++ baekend/src/test/java/com/example/recifit/RecifitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recifit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecifitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
