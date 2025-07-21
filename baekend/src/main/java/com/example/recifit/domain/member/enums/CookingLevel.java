366a3136f591f86f04e5e94d9d482849c8ed2269
beunyeong <beunyeong.b@gmail.com>
2025-07-21 16:39:10 +0900

    Feat: 회원가입 기능 구현 및 관련 Enum 클래스 추가

---
+++ baekend/src/main/java/com/example/recifit/domain/member/enums/CookingLevel.java
@@ -0,0 +1,12 @@
+package com.example.recifit.domain.member.enums;
+
+/**
+ *     BEGINNER: 초급, 요리를 처음 시작하는 단계
+ *     INTERMEDIATE: 중급, 기본적인 요리는 할 수 있는 수준
+ *     ADVANCED: 고급, 다양한 요리를 자신 있게 할 수 있는 단계
+ */
+public enum CookingLevel {
+    BEGINNER,
+    INTERMEDIATE,
+    ADVANCED
+}
---
+++ baekend/src/main/java/com/example/recifit/domain/member/enums/MemberType.java
@@ -0,0 +1,16 @@
+package com.example.recifit.domain.member.enums;
+
+/**
+ *     SINGLE: 자취생
+ *     COUPLE: 신혼/커플
+ *     FAMILY: 가족(부모님포함)
+ *     HOBBYIST: 취미 요리
+ *     PET_OWNER: 반려동물 보호자
+ */
+public enum MemberType {
+    SINGLE,
+    COUPLE,
+    FAMILY,
+    HOBBYIST,
+    PET_OWNER
+}
