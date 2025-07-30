ccee44ec882e62fdf066710f7829ffc6b08fc23b
beunyeong <beunyeong.b@gmail.com>
2025-07-31 04:17:02 +0900

    Feat: 저장공간(StorageLocation) enum 추가

---
+++ baekend/src/main/java/com/example/recifit/domain/ingredient/enums/StorageLocation.java
@@ -0,0 +1,13 @@
+package com.example.recifit.domain.ingredient.enums;
+
+/**
+ *     REFRIGERATED: 냉장 식품
+ *     FROZEN: 냉동 식품
+ *     ROOM_TEMPERATURE: 상온 보관
+ */
+public enum StorageLocation {
+
+    REFRIGERATED,
+    FROZEN,
+    ROOM_TEMPERATURE
+}
